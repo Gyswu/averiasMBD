49941b7f75a29389dc469ead84773aeb30a1e320
Gyswu <o.lugovskyy@gmail.com>
2020-11-04 10:14:43 +0100

    FIxed Usuario-Averias Relation

--- app/Model/orm/Usuarios/Usuario.php
+++ app/Model/orm/Usuarios/Usuario.php
@@ -3,12 +3,14 @@
 namespace App\Model\Orm;
 
 use Nextras\Orm\Entity\Entity;
+use Nextras\Orm\Relationships\OneHasMany;
 
 /**
  * Usuario
  *
  * @property    int    $id  {primary}
  * @property    Empresa       $empresa  {m:1 Empresa::$usuarios}
+ * @property    OneHasMany|Averias[] $averias {1:m Averias::$usuario}
  * @property    string|null   $nombre
  * @property    string|null   $apellidos
  * @property    string|null   $correo

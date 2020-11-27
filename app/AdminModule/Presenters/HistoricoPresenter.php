a59b6d5bdcc3c4306519ce289181c676b57b58a6
Jorge <jgas98@hotmail.com>
2020-11-27 11:26:50 +0100

    añadir historico

---
+++ app/AdminModule/Presenters/HistoricoPresenter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+
+use App\Forms\FormFactory;
+
+use App\Model\Orm\Historico;
+
+use Nette\Application\UI\Form;
+
+class HistoricoPresenter extends BaseAdminPresenter {}
---
+++ app/Presenters/HistoricoPresenter.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Presenters;
+
+use App\Forms\FormFactory;
+
+use App\Model\Orm\Historico;
+
+use Nette\Application\UI\Form;
+
+class HistoricoPresenter extends BasePresenter {
+    public function renderDefault(){
+        }
+}

b94c4070354427f5461ffd3b10ca3b647c1ae91a
VicHaunter Darkster <vichaunter@gmail.com>
2022-02-20 12:12:19 +0100

    chore(copias): findByPrinterId

--- app/Model/orm/Copias/CopiasRepository.php
+++ app/Model/orm/Copias/CopiasRepository.php
@@ -8,9 +8,16 @@ use Nextras\Orm\Repository\Repository;
  * @method Copias|NULL getById( $id )
  */
 
-class CopiasRepository extends Repository {
+class CopiasRepository extends Repository
+{
 
-    static function getEntityClassNames(): array {
-        return [ Copias::class ];
+    static function getEntityClassNames(): array
+    {
+        return [Copias::class];
+    }
+
+    public function findByPrinterId($printerId)
+    {
+        return $this->findBy(['maquina' => $printerId])->fetch();
     }
 }

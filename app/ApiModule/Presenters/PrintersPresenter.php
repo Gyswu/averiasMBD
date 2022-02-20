64687490c5b559d331ed4153d14c319e988c0bc0
VicHaunter Darkster <vichaunter@gmail.com>
2022-02-20 12:13:05 +0100

    feat(api): add printer info and printer copies updater

--- app/ApiModule/Presenters/PrintersPresenter.php
+++ app/ApiModule/Presenters/PrintersPresenter.php
@@ -24,10 +24,53 @@ final class PrintersPresenter extends BaseApiPresenter
         return $printer;
     }
 
+    /**
+     * Will update number of copies reported in the printer for given type
+     * 
+     * @param type {String} - type of report bn|color|lvl1-3|copies
+     * @param total {Integer}
+     * @param token {String}
+     */
+    public function actionUpdateReport()
+    {
+        $data = $this->getPostData();
+        $printer = $this->getPrinter();
+        $type = $data->type;
+
+        $copias = $this->orm->copias->findByPrinterId($printer->id);
+        if (!$copias) {
+            $this->sendErrors("No copies found for this printer");
+        }
+
+        //update only if given total is greater than previous (maybe is how to unprint pages in youtube xD)
+        if (!is_numeric($data->total) || $data->total <= $copias->$type) {
+            $this->sendErrors("You cannot unprint pages, total must be greater than the last one");
+        }
+
+        $copias->$type = $data->total;
+        $this->orm->copias->persistAndFlush($copias);
+
+        $this->sendJson(["status" => 1]);
+    }
+
     public function actionCodegroups()
     {
         $printer = $this->getprinter();
 
         $this->sendJson(Json::decode($printer->codegroups));
+    }
+
+    /**
+     * Gives printer info by token
+     * Will return only fields set on $whitelist to prevent data leaks
+     */
+    public function actionInfo()
+    {
+        $printer = $this->getprinter();
+        $info = $printer->toArray();
+        $info["codegroups"] = Json::decode($printer->codegroups);
+
+        $whitelist = ["id", "ip", "codegroups"];
+        $this->sendJson(array_intersect_key($info, array_flip($whitelist)));
     }
 }

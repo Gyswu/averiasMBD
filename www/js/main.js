c71bbb3067f1a21e53ba6414c79e979da491e79a
Gyswu <o.lugovskyy@gmail.com>
2022-02-24 02:30:38 +0100

    From navbar to sidebar
    Design change the main menu.From Navbar to Sidebar. W3Schools for the win!

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,17 @@
 $(function () {
     $.nette.init();
 });
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("myJuiNav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("myJuiNav").style.opacity = "1";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("myJuiNav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("myJuiNav").style.opacity = "0";
+}

ae4c1ea7c85efce1f0f1abb0667abd03251b7007
Gyswu <o.lugovskyy@gmail.com>
2022-03-10 00:27:46 +0100

    Visual Improvements
    Some visual Improvements

--- app/Model/Menu.php
+++ app/Model/Menu.php
@@ -11,41 +11,49 @@ class Menu {
         $menu = [
 
             [
+                'icon'    => 'bi bi-building',
                 'nombre'  => 'Clientes',
                 'mostrar' => $user->isAllowed(Roles::SECCION_EMPRESAS),
                 'nhref'   => 'Empresas:default',
             ],
             [
+                'icon' => 'bi bi-archive-fill',
                 'nombre'  => 'Proveedores',
                 'mostrar' => $user->isAllowed(Roles::SECCION_EMPRESAS),
                 'nhref'   => 'Proveedores:default',
             ],
             [
+                'icon' => 'bi bi-person-circle',
                 'nombre'  => 'Usuarios',
                 'mostrar' => $user->isAllowed(Roles::SECCION_USUARIOS),
                 'nhref'   => 'Usuarios:default',
             ],
 //            [
+//                'icon' => '',
 //                'nombre'  => 'Averias',
 //                'mostrar' => $user->isAllowed(Roles::SECCION_AVERIAS),
 //                'nhref'   => 'Averias:default',
 //            ],
             [
+                'icon' => 'bi bi-printer',
                 'nombre'  => 'Maquinas',
                 'mostrar' => $user->isAllowed(Roles::SECCION_AVERIAS),
                 'nhref'   => 'Maquinas:default',
             ],
             [
+                'icon' => 'bi bi-wrench',
                 'nombre'  => 'Cambios',
                 'mostrar' => $user->isAllowed(Roles::SECCION_AVERIAS),
                 'nhref'   => 'Piezas:default',
             ],
             [
+                'icon' => 'bi bi-info-circle-fill',
                 'nombre'  => 'Resumen',
                 'mostrar' => $user->isAllowed(Roles::SECCION_AVERIAS),
                 'nhref'   => 'Resumen:default',
             ],
             [
+                'icon' => 'bi bi-file-code-fill',
                 'nombre'  => 'Funciones',
                 'mostrar' => $user->isAllowed(Roles::SECCION_AVERIAS),
                 'nhref'   => 'Generalfunctions:default',
@@ -61,12 +69,14 @@ class Menu {
         $menu = [
 
             [
+                'icon' => '',
                 'nombre'  => 'Empresa',
                 'mostrar' => $user->isAllowed(Roles::SECCION_EMPRESAS),
                 'nhref'   => 'Empresas:default',
             ],
 
 //            [
+//                'icon' => '',
 //                'nombre'  => 'Averias',
 //                'mostrar' => $user->isAllowed(Roles::SECCION_AVERIAS),
 //                'nhref'   => 'Averias:default',
